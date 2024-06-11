adc6952c1d150b3f4ae80ca6cf411bc9a64a28c4
Filippo Ghibellini <f.ghibellini@gmail.com>
2024-06-11 21:34:48 +0200

    Clean up tx digest logic

--- zookeeper_txlog.py
+++ zookeeper_txlog.py
@@ -93,13 +93,9 @@ class TxLogReader:
 
     def read_int(self, label):
         bytes = self.input_stream.read(4) # TODO can it return less than 4 simply because it would require multiple page fetches?
-        if label == 'tx_len':
-            print(f"tx_len bytes: {bytes}")
         if len(bytes) < 4:
             raise ValueError(f"Tried to parse integer {label} but encountered EOF")
         val, = struct.unpack('>i', bytes)
-        if label == 'tx_len':
-            print(f"tx_len val: {val}")
         return val
 
     def read_long(self, label):
@@ -128,11 +124,14 @@ class TxLogReader:
             raise ValueError(f"Tried to parse buffer bytes for {label} but encountered EOF")
         return bytes
 
+    def current_position(self):
+        return self.input_stream.tell()
+
     def skip(self, size):
         pos0 = self.input_stream.tell()
         self.input_stream.seek(size, 1)
         pos1 = self.input_stream.tell()
-        if (pos1 - pos0) != size:
+        if (pos1 - pos0) != size: # TODO this check will not work ast seek always succeeds and tell() will just report the index beyond EOF
             raise RuntimeError(f"Could not skip {size} bytes!")
 
     def tx_crc_test(self, expected_crc_val, data_len):
@@ -143,7 +142,6 @@ class TxLogReader:
             return False
         crc = zlib.adler32(bytes)
         tx_end_marker = self.input_stream.read(1)
-        print(f"tx_end_marker: {tx_end_marker}")
         if tx_end_marker != b"\x42": # 'B'
             # raise RuntimeError(f"Incomplete transaction - no marker!")
             return False
@@ -158,22 +156,21 @@ def read_zookeeper_txlog(file_path):
         magic = txlog_reader.read_int('magic')
         version = txlog_reader.read_int('version')
         db_id = txlog_reader.read_long('db_id')
-        if magic != 1514884167:
+        if magic != 1514884167: # "ZKLG"
             raise RuntimeError(f"Invalid file magic. Expected 1514884167 (\"ZKLG\") but got : {magic}")
         if version != 2:
             raise RuntimeError(f"Invalid txlog version. Expected 2 but got : {version}")
         # TODO 0 ?
         #if db_id != -1:
         #    raise RuntimeError(f"Invalid txlog DB_ID. Expected -1 but got : {db_id}")
-        print("ALL DONE!")
 
         log_transactions = []
         while True:
             tx_crc = txlog_reader.read_long('tx_crc')
             tx_len = txlog_reader.read_int('tx_len')
-            print(f"tx len: {tx_len}")
             if not txlog_reader.tx_crc_test(tx_crc, tx_len):
-                break
+                break # this is how we know we reached the end of the txLog
+            tx_start_pos = txlog_reader.current_position()
             tx_client_id = txlog_reader.read_long('tx_client_id')
             tx_cxid = txlog_reader.read_int('tx_cxid')
             tx_zxid = txlog_reader.read_long('tx_zxid')
@@ -186,10 +183,9 @@ def read_zookeeper_txlog(file_path):
                 "time": tx_time,
                 "type": tx_type
             }
+            # TODO other opcodes?
             if tx_type == TxOpCodes.ERROR.value:
                 exception_code = txlog_reader.read_int('err')
-                # TODO how come there is no digest here but there is in the other tx_types?!
-                txlog_reader.skip(1) # end of transaction marker 0x42
                 exception_opcode = exception_code_lookup_dict[exception_code]
                 tx = {
                     'type': 'ERROR',
@@ -199,9 +195,6 @@ def read_zookeeper_txlog(file_path):
                 path = txlog_reader.read_string('path')
                 data = txlog_reader.read_buffer('data')
                 tx_set_data_version = txlog_reader.read_int('version')
-                tx_digest_version = txlog_reader.read_int('tx_digest_version')
-                tx_digest = txlog_reader.read_long('tx_digest')
-                txlog_reader.skip(1) # end of transaction marker 0x42
                 tx = {
                     'type': 'SET_DATA',
                     'path': path,
@@ -228,9 +221,6 @@ def read_zookeeper_txlog(file_path):
                         })
                 ephemeral = txlog_reader.read_bool('ephemeral')
                 parent_cversion = txlog_reader.read_int('parent_cversion')
-                tx_digest_version = txlog_reader.read_int('tx_digest_version')
-                tx_digest = txlog_reader.read_long('tx_digest')
-                txlog_reader.skip(1) # end of transaction marker 0x42
                 tx = {
                     'type': 'CREATE2',
                     'path': path,
@@ -240,9 +230,6 @@ def read_zookeeper_txlog(file_path):
                 }
             elif tx_type == TxOpCodes.CREATE_SESSION.value:
                 timeout = txlog_reader.read_int('timeout')
-                tx_digest_version = txlog_reader.read_int('tx_digest_version')
-                tx_digest = txlog_reader.read_long('tx_digest')
-                txlog_reader.skip(1) # end of transaction marker 0x42
                 tx = {
                     'type': 'CREATE_SESSION',
                     'timeout': timeout
@@ -255,18 +242,12 @@ def read_zookeeper_txlog(file_path):
                     for i in range(paths_2_delete_len):
                         path = txlog_reader.read_string('path2delete')
                         paths_2_delete.append(path)
-                tx_digest_version = txlog_reader.read_int('tx_digest_version')
-                tx_digest = txlog_reader.read_long('tx_digest')
-                txlog_reader.skip(1) # end of transaction marker 0x42
                 tx = {
                     'type': 'CLOSE_SESSION',
                     'paths2Delete': paths_2_delete
                 }
             elif tx_type == TxOpCodes.DELETE.value:
                 path = txlog_reader.read_string('path')
-                tx_digest_version = txlog_reader.read_int('tx_digest_version')
-                tx_digest = txlog_reader.read_long('tx_digest')
-                txlog_reader.skip(1) # end of transaction marker 0x42
                 tx = {
                     'type': 'DELETE',
                     'path': path
@@ -283,9 +264,6 @@ def read_zookeeper_txlog(file_path):
                             'type': tx_type,
                             'data': '...'
                         })
-                tx_digest_version = txlog_reader.read_int('tx_digest_version')
-                tx_digest = txlog_reader.read_long('tx_digest')
-                txlog_reader.skip(1) # end of transaction marker 0x42
                 tx = {
                     'type': 'MULTI',
                     'txns': txns
@@ -295,17 +273,24 @@ def read_zookeeper_txlog(file_path):
             # print(json.dumps(tx, indent=4))
             # TODO V0 create
 
-            digest = None
+            tx_end_pos = txlog_reader.current_position()
+            processed_tx_len =  tx_end_pos - tx_start_pos
+            unprocessed_byte_count = tx_len - processed_tx_len
 
+            digest = None
             # TODO zookeeper.digest.enabled
-            # if tx_digest_enabled
-            if False:
+            # The digest is available only on some of the transaction (e.g. ERROR txs lack it).
+            # You know if it's available by checking whether there is unprocessed data within
+            # the transaction length after parsing the transaction record.
+            if unprocessed_byte_count >= 12: # 12 bytes of digest
                 tx_digest_version = txlog_reader.read_int("tx_digest_version")
                 tx_tree_digest = txlog_reader.read_long("tx_tree_digest")
                 digest = {
                     'version': tx_digest_version,
                     'tree_digest': tx_tree_digest
                 }
+
+            txlog_reader.skip(1) # end of transaction marker 0x42
             
             tx_rec = {
                 'tx': tx,
@@ -321,4 +306,4 @@ def read_zookeeper_txlog(file_path):
             
         
 
-read_zookeeper_txlog("/Users/fghibellini/Downloads/log.95e000ebfc3")
+read_zookeeper_txlog("/Users/fghibellini/Downloads/log.95e000d8b9e")
